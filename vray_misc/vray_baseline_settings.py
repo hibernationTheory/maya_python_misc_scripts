c8896f160c326a2342d772c045f80bac37938398
Engin Arslan <hibernationtheory@gmail.com>
2014-05-15 22:06:14 -0400

    small fixes to the vray_baseline_settings.py

--- vray_misc/vray_baseline_settings.py
+++ vray_misc/vray_baseline_settings.py
@@ -2,15 +2,15 @@ import maya.cmds as cmds
 myRenderer = "vraySettings"
 
 settings = [
-("sRGBOn", 1)
+("sRGBOn", 1),
 ("vfbOn", 1),
 ("globopt_light_doDefaultLights", 0),
 ("globopt_mtl_limitDepth", 1),
-("globopt_mtl_maxDepth", 5)
+("globopt_mtl_maxDepth", 5),
 ("samplerType", 1),
 #("aaFilterType", 6),
 #("aaFilterSize", 2),
-("dmcLockThreshold", 1)
+("dmcLockThreshold", 1),
 ("cmap_type", 1),
 ("cmap_gamma", 2.2),
 ("cmap_subpixelMapping", 1),
@@ -24,8 +24,7 @@ settings = [
 ("ddisplac_maxSubdivs", 4),
 ("sys_rayc_dynMemLimit", 18000),
 ("sys_regsgen_xc", 16),
-
 ]
 
-for item in settings():
-    cmds.setAttr("%s.%s" %(myRenderer, item[0]), item[1][0])
+for item in settings:
+    cmds.setAttr("%s.%s" %(myRenderer, item[0]), item[1])
