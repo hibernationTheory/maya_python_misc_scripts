6caa780c24c8e4a07c983f024431b5b7cc205c65
Engin Arslan <hibernationtheory@gmail.com>
2014-05-15 21:49:13 -0400

    updates the vray launch script
    updates the vray launch script to load the vray plugin as well

--- vray_misc/vray_launch_vray.py
+++ vray_misc/vray_launch_vray.py
@@ -1,6 +1,10 @@
 import maya.cmds as cmds
 import maya.mel as mel
 
+cmds.loadPlugin("vrayformaya")
+
 renderers = ["mayaSoftware", "vray"]
+
 myRenderer = renderers[1]
 mel.eval("setCurrentRenderer %s" %myRenderer)
+print "renderer is set to %s" % myRenderer
